4532570a417ffc70da374659cad4da47368cd90c
imomoh23 <imomoh23@gmail.com>
2024-11-15 19:03:47 -0600

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "ec2_instance" {
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group_jenkins.id]
   key_name               = "devopskeypair"
-  # user_data            = file("install_jenkins.sh")
+  user_data            = "${file("install_jenkins.sh")}"
 
   tags = {
     Name = "jenkins_server"
@@ -99,33 +99,6 @@ resource "aws_instance" "ec2_instance" {
 
 
 # an empty resource block
-resource "null_resource" "name" {
-
-  # ssh into the ec2 instance 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
-
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
-  provisioner "file" {
-    source      = "install_jenkins.sh"
-    destination = "/tmp/install_jenkins.sh"
-  }
-
-  # set permissions and run the install_jenkins.sh file
-  provisioner "remote-exec" {
-    inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
-    ]
-  }
-
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
-}
 
 
 # print the url of the jenkins server
